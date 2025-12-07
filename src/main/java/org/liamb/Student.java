1ac28256c92ea194cc4a523cb88ff7f395645624
liamb2535004 <lbohdjal@gmail.com>
2025-12-07 14:11:24 -0500

    added Student class constructor

--- src/main/java/org/liamb/Student.java
+++ src/main/java/org/liamb/Student.java
@@ -1,6 +1,7 @@
 package org.liamb;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Student {
     private String studentId;
@@ -8,8 +9,17 @@ public class Student {
     private Gender gender;
     private Address address;
     private Department department;
-    private ArrayList<Course> registeredCourses;
+    private List<Course> registeredCourses;
     private static int nextId = 1;
+
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentId = String.format("S%06d", nextId++);
+        this.studentName = studentName;
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.registeredCourses = new ArrayList<>();
+    }
 
     public enum Gender {
         FEMALE,
