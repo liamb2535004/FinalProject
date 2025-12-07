006f669b95bbc448fb6a9d5e602b9f502d06070e
liamb2535004 <lbohdjal@gmail.com>
2025-12-07 12:49:02 -0500

    added calcAssignmentAvg method

--- src/main/java/org/liamb/Assignment.java
+++ src/main/java/org/liamb/Assignment.java
@@ -8,4 +8,12 @@ public class Assignment {
     private double weight;
     private ArrayList<Integer> scores;
     private static int nextId = 1;
+
+    void calcAssignmentAvg() {
+        int sum = 0;
+        for (int score : scores) {
+            sum += score;
+        }
+        int avg = sum / scores.size();
+    }
 }
