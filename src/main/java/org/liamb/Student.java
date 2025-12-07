775034fe4a1fb1cb7975c17c1882e08a5a1284bd
liamb2535004 <lbohdjal@gmail.com>
2025-12-07 14:02:31 -0500

    Student class fields and Gender enum

---
+++ src/main/java/org/liamb/Student.java
@@ -0,0 +1,18 @@
+package org.liamb;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+    private static int nextId = 1;
+
+    public enum Gender {
+        FEMALE,
+        MALE
+    }
+}
