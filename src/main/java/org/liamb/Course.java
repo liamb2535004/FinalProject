4f23160524a1fe603f47c3c923b4ad03e1ecf667
liamb2535004 <lbohdjal@gmail.com>
2025-12-13 16:50:11 -0500

    add registerStudent method to Course class

--- src/main/java/org/liamb/Course.java
+++ src/main/java/org/liamb/Course.java
@@ -11,6 +11,7 @@ public class Course {
     private List<Assignment> assignments;
     private List<Student> registeredStudents;
     private static int nextId = 1;
+    private List<Double> finalScores;
 
     public Course(String courseName, double credits, Department department) {
         this.courseId = String.format("C-%s-%02d", department.getDepartmentId(), nextId++);
@@ -27,5 +28,19 @@ public class Course {
             totalWeight += assignment.getWeight();
         }
         return (totalWeight == 100);
+    }
+
+    public boolean registerStudent(Student student) {
+        if (student == null || this.registeredStudents.contains(student)) {
+            return false;
+        }
+        this.registeredStudents.add(student);
+        this.finalScores.add(-1.0);
+
+        for (Assignment assignment : this.assignments) {
+            List<Integer> assignmentScores = assignment.getScores();
+            assignmentScores.add(-1);
+        }
+        return true;
     }
 }
