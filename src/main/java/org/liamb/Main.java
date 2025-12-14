cdcfa829827c90070a9b87eb2ea26b0482a0f202
liamb2535004 <lbohdjal@gmail.com>
2025-12-14 18:06:37 -0500

    displayScores, toString methods and toSimplifiedString methods tested in Main class

--- src/main/java/org/liamb/Main.java
+++ src/main/java/org/liamb/Main.java
@@ -1,17 +1,39 @@
 package org.liamb;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.nio.charset.CoderResult;
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Test displayScores() & toString & toSimplifiedString
+        Department math = new Department("math");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Course programming = new Course("programming", 2.5, math);
+
+        Student student1 = new Student("ethan collins", Student.Gender.MALE,
+                new Address(1, "street", "city", Address.Province.QC, "A1B2C3"), math);
+        Student student2 = new Student("lucas bennett", Student.Gender.MALE,
+                new Address(1, "street", "city", Address.Province.QC, "A1B2C3"), math);
+        Student student3 = new Student("ava harrington", Student.Gender.MALE,
+                new Address(1, "street", "city", Address.Province.QC, "A1B2C3"), math);
+
+        student1.registerCourse(programming);
+        student2.registerCourse(programming);
+        student3.registerCourse(programming);
+
+        programming.addAssignment("Assignment01", 10);
+        programming.addAssignment("Assignment02", 10);
+        programming.addAssignment("Assignment03", 10);
+        programming.addAssignment("Exam01", 35);
+        programming.addAssignment("Exam02", 35);
+
+        programming.generateScores();
+        programming.displayScores();
+
+        System.out.println(student1.toString());
+        System.out.println(student1.toSimplifiedString());
+        System.out.println(programming.toString());
+        System.out.println(programming.toSimplifiedString());
+
     }
 }
